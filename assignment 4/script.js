0d3f6f25c9984ffeff0828850acb003c9a68170a
Daswadhama <103351156+Daswadhama@users.noreply.github.com>
2023-03-24 15:04:51 +0530

    updated

---
+++ assignment 4/script.js
@@ -0,0 +1,20 @@
+var name=new Array();
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[0]="larry";
+names[0]="paula";
+names[0]="laura";
+names[0]="jim";
+
+for (var i = 0; i < names.length; i++) {
+	if(names[i].charAt(0)==='J')|| names[i].charAt(0)==='j'0){
+        console.log("Goodbye "+ name[i])
+    }
+else{
+	console.log("Hello "+ name[i])
+    }
+}
